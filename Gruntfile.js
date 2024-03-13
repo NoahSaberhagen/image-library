6abee6032622ae82191f878c4f68170245b6c35e
NoahSaberhagen <noahsaberhagen@gmail.com>
2024-03-13 16:39:04 -0400

    grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,8 +6,19 @@ module.exports = function(grunt) {
         src: 'src/js/*.js',
         dest: 'dist/js/scripts.js'
       }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/styles/styles.css': 'src/scss/styles.scss',
+        }
+      }
     }
   })
 
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
 }
