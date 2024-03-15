a7df519ef8cfc937296a34193bf09303d08361d3
NoahSaberhagen <noahsaberhagen@gmail.com>
2024-03-15 17:11:31 -0400

    adding icon

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,13 @@
-
 // Append new image components, then once they are in HTML set each component style
 // to provide background images.
 const grid = document.querySelector('#grid');
 let gridInnerHtml = '';
+
+const icon = `
+  <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-m9simb" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="CropFreeIcon">
+    <path d="M3 5v4h2V5h4V3H5c-1.1 0-2 .9-2 2m2 10H3v4c0 1.1.9 2 2 2h4v-2H5zm14 4h-4v2h4c1.1 0 2-.9 2-2v-4h-2zm0-16h-4v2h4v4h2V5c0-1.1-.9-2-2-2">
+    </path>
+  </svg>`
 
 for (let i = 0; i < images.length; i++) {
 
@@ -13,12 +18,14 @@ for (let i = 0; i < images.length; i++) {
   // span is for accessibility
   gridInnerHtml += `
     <div id="container${i}">
+      ${icon}
       <input type="checkbox" class="btn-check" id="btncheck${i}" autocomplete="off">
       <label class="btn" for="btncheck${i}" id="image${i}"></label>
       <span class="visually-hidden">${images[i].alt}</span>
     </div>
   `
-}
+};
+
 grid.innerHTML = gridInnerHtml;
 
 for (let i = 0; i < images.length; i++) {
@@ -27,4 +34,4 @@ for (let i = 0; i < images.length; i++) {
   currentImage.setAttribute("style", `
     background-image: url('${images[i].src}');
   `)
-}
+};
